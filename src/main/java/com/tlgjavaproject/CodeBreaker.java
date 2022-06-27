99917fa06ddf6b15832cbb3ca559582c22dc3a7a
henrywg <henrywg@amazon.com>
2022-06-27 12:06:19 -0700

    Added TODOs.

--- src/main/java/com/tlgjavaproject/CodeBreaker.java
+++ src/main/java/com/tlgjavaproject/CodeBreaker.java
@@ -1,4 +1,27 @@
 package com.tlgjavaproject;
 
 public class CodeBreaker {
+    //TODO Generate the answer, four digit non-duplicated, *use Map?;
+
+    //TODO Print out instruction for the player, and give them the
+    // block enter their first guess;
+
+    //TODO link the guess to program;
+
+    //TODO i = 7, use i-- the guess, go back to loop;
+
+    //TODO check the guess, *use if/else, getValue from Map list? and loop back for
+    // next guess;
+
+    //TODO When i == 0 , print lose statement and answer.
+    //TODO When guess is correct, print win statement.
+
+    //TODO Optionals:
+    //TODO Use pop up windows for all;
+    //TODO Print out guesses left and full history with %iA%iB
+
+
+
+
+
 }
