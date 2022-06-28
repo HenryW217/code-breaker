70e9b969be4c49dccaae0b4f45eb1b59ac8ac945
henrywg <henrywg@amazon.com>
2022-06-28 11:06:49 -0700

    Added input generator, it works too.

--- src/main/java/com/tlgjavaproject/CodeBreaker.java
+++ src/main/java/com/tlgjavaproject/CodeBreaker.java
@@ -7,14 +7,13 @@ import java.util.Scanner;
 public class CodeBreaker {
     /*Terms:
     The system generated 4 digit #, ref as "answer";
-    If we are able to use Map, use w,x,y,z as value of Map;
     Use "i" for guesses count, the reason we count down is for future usage if we print "lives left";
     playerEntry is the guesses the player entered;
 
      */
     private static int guessLeft = 7;
     private static ArrayList answer = new ArrayList();
-    private static ArrayList guessNum = new ArrayList();
+    private static ArrayList input = new ArrayList();
 
 
     public static void main(String[] args) {
@@ -25,8 +24,9 @@ public class CodeBreaker {
         int[] attemptScore = new int[3];  //The score of the latest attempt.
 
 
+
         CodeBreaker playerGuess = new CodeBreaker();
-        // I took the ones you entered, should we add \n for last three lines?
+
         System.out.println("CODE BREAKER");
         System.out.println("(Try to break the code.)");
         System.out.println("\nINSTRUCTIONS");
@@ -35,57 +35,6 @@ public class CodeBreaker {
         System.out.println("- Attempt to break the code by entering a four-digit number that contains four different digits matching those listed above.");
         System.out.println("\nPlease enter a four-digit number below then click 'Enter', no duplicated numbers.");
         System.out.println("You have 7 opportunities to win the game. Let's play!");
-
-        //TODO Print out instruction for the player, and give them the
-        // block enter their first guess; (Paul)
-
-        Scanner console = new Scanner(System.in);
-        while (console.hasNextLine() && guessLeft < 7) {
-            System.out.println("Guess any number in the code");
-            String guess = console.nextLine();
-            while ((numGuess <= guessLeft) && (!codesBroken)) {
-                System.out.println("\nATTEMPT #" + numGuess);
-                System.out.print("Enter a 4-digit code: ");
-
-        //TODO i = 7, use i-- the guess, go back to loop; (Paul)
-        //TODO check the guess, *use if/else, getValue from Map list? and loop back for
-        // next guess; (Paul)
-                //Generated some if/else statements. Need to figure out how to  link to Array List.
-                if (attemptScore[0] != 1) {
-                    System.out.print("SCORE: " + attemptScore[0] + " exact matches");
-                }
-                if (attemptScore[1] != 1) {
-                    System.out.print(", " + attemptScore[1] + " near match");
-                }
-                if (attemptScore[2] != 1) {
-                    System.out.print(", " + attemptScore[2] + " miss");
-                }
-                if (attemptScore[0] < 4) {
-                    attemptsRemaining--;
-                    if (attemptsRemaining > 1) {
-                        System.out.println("\n" + attemptsRemaining + " attempts left!");
-                    } else if (attemptsRemaining == 1) {
-                        System.out.println("\n" + attemptsRemaining + " attempt left! Your next attempt MUST be correct.");
-                    } else {
-                        System.out.println("\n" + attemptsRemaining + " attempts left! You have run out of attempts.");
-        //TODO When i == 0 , print lose statement and answer.(Paul)
-                        System.out.println("You did not break the code.");
-                        //Thank player for playing.
-                        System.out.println("\nTHANKS FOR PLAYING.");
-                    }
-                    numGuess++;
-                }
-        //TODO When guess is correct, print win statement.(Paul)
-                //If the player breaks the code, congratulate the player.
-                else {
-                    codesBroken = true;
-                    System.out.println("\n\nWELL DONE!");
-                    System.out.println("YOU BROKE THE CODE!");
-                    //Thank player for playing.
-                    System.out.println("\nTHANKS FOR PLAYING.");
-                }
-            }
-        }
 
         //TODO Generate the answer, four digit non-duplicated, *use Map?; (Henry)
         ArrayList list = new ArrayList();    // To generate an Array list from 0 to 9;
@@ -96,11 +45,79 @@ public class CodeBreaker {
         Collections.shuffle(list); // Shuffle the list;
         for (int a = 0; a < 4; a++) {
             answer.add(list.get(a));
-            //System.out.println(answer); // TODO !!!This line is to test only, remove after code complete!!!
+            System.out.println(answer); // TODO !!!This line is to test only, remove after code complete!!!
+            input.clear(); // Need to ask Nick, won't clear previous entry.
         }
+
+        // Henry added: use Scanner to take input to a list.
+        ArrayList input = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < 4; i++){
+            input.add(sc.nextInt());
+            System.out.println(input);// TODO !!!This line is to test only, remove after code complete!!!
+            input.clear();
+        }
+
+
+
+
+//
+//
+//        //TODO Give player the block enter their first guess; (Paul)
+//
+//        Scanner console = new Scanner(System.in);
+//        while (console.hasNextLine() && guessLeft < 7) {
+//            System.out.println("Guess any number in the code");
+//            String guess = console.nextLine();
+//            while ((numGuess <= guessLeft) && (!codesBroken)) {
+//                System.out.println("\nATTEMPT #" + numGuess);
+//                System.out.print("Enter a 4-digit code: ");
+//
+//        //TODO i = 7, use i-- the guess, go back to loop; (Paul)
+//        //TODO check the guess, *use if/else, getValue from Map list? and loop back for
+//        // next guess; (Paul)
+//                //Generated some if/else statements. Need to figure out how to  link to Array List.
+//                if (attemptScore[0] != 1) {
+//                    System.out.print("SCORE: " + attemptScore[0] + " exact matches");
+//                }
+//                if (attemptScore[1] != 1) {
+//                    System.out.print(", " + attemptScore[1] + " near match");
+//                }
+//                if (attemptScore[2] != 1) {
+//                    System.out.print(", " + attemptScore[2] + " miss");
+//                }
+//                if (attemptScore[0] < 4) {
+//                    attemptsRemaining--;
+//                    if (attemptsRemaining > 1) {
+//                        System.out.println("\n" + attemptsRemaining + " attempts left!");
+//                    } else if (attemptsRemaining == 1) {
+//                        System.out.println("\n" + attemptsRemaining + " attempt left! Your next attempt MUST be correct.");
+//                    } else {
+//                        System.out.println("\n" + attemptsRemaining + " attempts left! You have run out of attempts.");
+//        //TODO When i == 0 , print lose statement and answer.(Paul)
+//                        System.out.println("You did not break the code.");
+//                        //Thank player for playing.
+//                        System.out.println("\nTHANKS FOR PLAYING.");
+//                    }
+//                    numGuess++;
+//                }
+//        //TODO When guess is correct, print win statement.(Paul)
+//                //If the player breaks the code, congratulate the player.
+//                else {
+//                    codesBroken = true;
+//                    System.out.println("\n\nWELL DONE!");
+//                    System.out.println("YOU BROKE THE CODE!");
+//                    //Thank player for playing.
+//                    System.out.println("\nTHANKS FOR PLAYING.");
+//                }
+//            }
+//        }
+
+
     }
 
-    //TODO Optionals: TBD
+    //TODO Optionals:
     //TODO Use pop up windows for all;
     //TODO Print out guesses left and full history with %dA%dB
 }
