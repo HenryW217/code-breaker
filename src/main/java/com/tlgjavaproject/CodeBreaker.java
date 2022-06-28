6acd5d75d14fb808dd4c4e309b0d9f2b5f8af277
ngonpaul <pmgonzalez05@outlook.com>
2022-06-28 01:46:28 -0700

    Added a squiggly bracket at the end that was missing to CodeBreaker.java

--- src/main/java/com/tlgjavaproject/CodeBreaker.java
+++ src/main/java/com/tlgjavaproject/CodeBreaker.java
@@ -62,9 +62,8 @@ public class CodeBreaker {
             System.out.println(answer); // TODO !!!This line is to test only, remove after code complete!!!
         }
 
-    //TODO Print out instruction for the player, and give them the
-    // block enter their first guess; (Paul)
-
+        //TODO Print out instruction for the player, and give them the
+        // block enter their first guess; (Paul)
 
 
         //TODO link the guess to program;
@@ -76,48 +75,47 @@ public class CodeBreaker {
 
         //Generated some if/else statements. Need to figure out how to  link to Array List.
         public static void code() {
-        while ((attemptNumber <= guessLeft) && (!codesBroken)) {
-            System.out.println("\nATTEMPT #" + attemptNumber);
-            System.out.print("Enter a 4-digit code: ");
+            while ((attemptNumber <= guessLeft) && (!codesBroken)) {
+                System.out.println("\nATTEMPT #" + attemptNumber);
+                System.out.print("Enter a 4-digit code: ");
 
-            //TODO When i == 0 , print lose statement and answer.(Paul)
-            if (attemptScore[0] != 1) {
-                System.out.print("SCORE: " + attemptScore[0] + " exact matches");
-            }
-            if (attemptScore[1] != 1) {
-                System.out.print(", " + attemptScore[1] + " near match");
-            }
-            if (attemptScore[2] != 1) {
-                System.out.print(", " + attemptScore[2] + " miss");
-            }
-            if (attemptScore[0] < 4) {
-                attemptsRemaining--;
-                if (attemptsRemaining > 1) {
-                    System.out.println("\n" + attemptsRemaining + " attempts left!");
-                } else if (attemptsRemaining == 1) {
-                    System.out.println("\n" + attemptsRemaining + " attempt left! Your next attempt MUST be correct.");
-                } else {
-                    System.out.println("\n" + attemptsRemaining + " attempts left! You have run out of attempts.");
-                    System.out.println("You did not break the code.");
+                //TODO When i == 0 , print lose statement and answer.(Paul)
+                if (attemptScore[0] != 1) {
+                    System.out.print("SCORE: " + attemptScore[0] + " exact matches");
                 }
-                attemptNumber++;
+                if (attemptScore[1] != 1) {
+                    System.out.print(", " + attemptScore[1] + " near match");
+                }
+                if (attemptScore[2] != 1) {
+                    System.out.print(", " + attemptScore[2] + " miss");
+                }
+                if (attemptScore[0] < 4) {
+                    attemptsRemaining--;
+                    if (attemptsRemaining > 1) {
+                        System.out.println("\n" + attemptsRemaining + " attempts left!");
+                    } else if (attemptsRemaining == 1) {
+                        System.out.println("\n" + attemptsRemaining + " attempt left! Your next attempt MUST be correct.");
+                    } else {
+                        System.out.println("\n" + attemptsRemaining + " attempts left! You have run out of attempts.");
+                        System.out.println("You did not break the code.");
+                    }
+                    attemptNumber++;
+                }
+                //If the player breaks the code, congratulate the player.
+                else {
+                    codesBroken = true;
+                    System.out.println("\n\nWELL DONE!");
+                    System.out.println("YOU BROKE THE CODE!");
+                }
             }
-            //If the player breaks the code, congratulate the player.
-            else {
-                codesBroken = true;
-                System.out.println("\n\nWELL DONE!");
-                System.out.println("YOU BROKE THE CODE!");
-            }
+
+            //TODO When guess is correct, print win statement.(Paul)
+            //Thank player for playing.
+            System.out.println("\nTHANKS FOR PLAYING.");
         }
 
-        //TODO When guess is correct, print win statement.(Paul)
-        //Thank player for playing.
-        System.out.println("\nTHANKS FOR PLAYING.");
+        //TODO Optionals:
+        //TODO Use pop up windows for all;
+        //TODO Print out guesses left and full history with %dA%dB
     }
-
-    //TODO Optionals:
-    //TODO Use pop up windows for all;
-    //TODO Print out guesses left and full history with %dA%dB
-
-
 }
