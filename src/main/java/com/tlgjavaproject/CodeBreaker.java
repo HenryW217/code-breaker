f136162de709bca029b4de5b55f307835242a468
henrywg <henrywg@amazon.com>
2022-06-30 09:17:37 -0700

    All tasks complete. Here we go. Code Breaker v 1.0.

--- src/main/java/com/tlgjavaproject/CodeBreaker.java
+++ src/main/java/com/tlgjavaproject/CodeBreaker.java
@@ -10,7 +10,7 @@ public class CodeBreaker {
     private static int aCount = 0;
     private static int bCount = 0;
 
-
+// Print out welcome message to give the player instructions.
     void welcomeMessage() {
         System.out.println("CODE BREAKER");
         System.out.println("(Try to break the code.)");
@@ -22,31 +22,52 @@ public class CodeBreaker {
         System.out.println("You have 7 opportunities to win the game. Let's play!");
     }
 
+    // Generate a random four-digit number in the memory as the answer for player to guess
+    // by creating a 0-9 list, shuffle them, then take the first four digits.
     void answerGenerator() {
-        ArrayList<Integer> list = new ArrayList<>();    // To generate an Array list from 0 to 9;
-        for (int l = 0; l < 10; l++)    // "l" for the 0 - 9 list;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int l = 0; l < 10; l++)
             list.add(l);
 
-        Collections.shuffle(list); // Shuffle the list;
+        Collections.shuffle(list);
+
         for (int a = 0; a < 4; a++) {
             answer.add(list.get(a));
         }
     }
 
-    // Create method to get input Array list.
+    // Convert player's input into an Array list.
+    // at mean time, check to make sure
+    //                  - player entered numbers;
+    //                  - player's input is a four-digit number;
     void playerEntry() {
         Scanner sc = new Scanner(System.in);
-        int entry = sc.nextInt();
+        int entry;
+
+        while (!sc.hasNextInt()) {
+            System.out.println("Invalidated input, please enter a four-digit NUMBER.");
+            sc.next();
+        }
+        entry = sc.nextInt();
+        int entryLength = String.valueOf(entry).length();
+
+        while (entryLength != 4) {
+            System.out.println("Invalidated input, please enter a FOUR-digit number.");
+            // I have a feeling that these following two line could be re-initialized
+            // in another way, I tried, but break the code. So I'll leave it as is for now.
+            entry = sc.nextInt();
+            entryLength = String.valueOf(entry).length();
+        }
 
         for (int i = 0; i < 4; i++) {
             input.add(0, entry % 10);
             entry = entry / 10;
         }
-//            System.out.println("***Test purpose, player entry is: " + input + " ***");// TODO !!!This line is to test only, remove after code complete!!!
     }
 
+    // This is the method to check if there are any duplicates.
+    // Could be built into playerEntry, will try if we got time left.
     static boolean inputValidator() {
-
         for (int j = 0; j < CodeBreaker.input.size(); j++)
             for (int k = j + 1; k < CodeBreaker.input.size(); k++)
 
@@ -56,7 +77,10 @@ public class CodeBreaker {
         return false;
     }
 
-    // Create compare result method.
+    // This is to compare player's input to the answer;
+    // Check how many number are there with right location;
+    // Then check how many number are correct;
+    // Print out feedback to player.
     void compareInput() {
         for (int i = 0; i < 4; i++)
             if (Objects.equals(answer.get(i), input.get(i)))
@@ -70,10 +94,8 @@ public class CodeBreaker {
             System.out.printf("%dA%dB", aCount, bCount);
             System.out.println();
         }
-
         aCount = 0;
         bCount = 0;
     }
-
 
 }
